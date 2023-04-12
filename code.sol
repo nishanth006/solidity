3d347134a4a524c4e3ef8a2b1e050e0cc1316429
nishanth006 <118667618+nishanth006@users.noreply.github.com>
2023-04-12 22:49:00 +0530

    Add files via upload

---
+++ code.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+// Version of Solidity compiler this program was written for
+
+pragma solidity >=0.5.0 <0.7.0;
+contract Add {
+    function add(uint x,uint y)public pure returns (uint){
+        return x+y;
+    }
+    
+}
